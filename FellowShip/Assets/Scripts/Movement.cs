e2845950a8770585cd1894e2a7125e4929d1aaff
martijnk1992 <Martijn.Koekkoek2@hva.nl>
2015-11-30 13:20:48 +0100

    movement

--- FellowShip/Assets/Scripts/Movement.cs
+++ FellowShip/Assets/Scripts/Movement.cs
@@ -3,17 +3,55 @@ using System.Collections;
 
 public class Movement : MonoBehaviour
 {
+    public Rigidbody boat;
 
-    public GameObject boat;
+	float speed;
+
+
 	// Use this for initialization
 	void Start ()
     {
-	    
+		boat = GetComponent<Rigidbody>();
+		speed = 0;
 	}
 	
 	// Update is called once per frame
 	void Update ()
-    {
-	
+	{
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			boat.velocity = new Vector3(-10,0,0);
+		}
+		else if(Input.GetKeyUp(KeyCode.A))
+		{
+			boat.velocity = new Vector3(0,0,0);
+		}
+
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			boat.velocity = new Vector3(10,0,0);
+		}
+		else if(Input.GetKeyUp(KeyCode.D))
+		{
+			boat.velocity = new Vector3(0,0,0);
+		}
+
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			boat.velocity = new Vector3(0,0,10);
+		}
+		else if(Input.GetKeyUp(KeyCode.W))
+		{
+			boat.velocity = new Vector3(0,0,0);
+		}
+		
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			boat.velocity = new Vector3(0,0,-10);
+		}
+		else if(Input.GetKeyUp(KeyCode.S))
+		{
+			boat.velocity = new Vector3(0,0,0);
+		}
 	}
 }
