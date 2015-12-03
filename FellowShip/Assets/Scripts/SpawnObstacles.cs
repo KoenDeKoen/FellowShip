4a6e5d46a26120b2ecd225059c889423fcc89463
KoenDeKoen <koen.brouwers@live.nl>
2015-12-03 14:15:49 +0100

    NoConflictsPls

---
+++ FellowShip/Assets/Scripts/SpawnObstacles.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnObstacles : MonoBehaviour
+{
+    public Obstacles obstacles;
+    private List<Vector3> spawnedobstacles;
+	// Use this for initialization
+	void Start ()
+    {
+        spawnNextObstacle();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    public void spawnNextObstacle()
+    {
+        GameObject spawnedobstacle = Instantiate(obstacles.returnObstacles()[Random.Range(0, obstacles.returnObstacles().Count)]);
+
+    }
+}
