9e2c1dc8ab533ad066e00290830835db725cb10b
KoenDeKoen <koen.brouwers@live.nl>
2015-12-08 13:43:10 +0100

    Dunno

--- FellowShip/Assets/Scripts/Pickup.cs
+++ FellowShip/Assets/Scripts/Pickup.cs
@@ -6,6 +6,12 @@ public class Pickup : MonoBehaviour
     public GameObject pickupmodel;
     private GameObject currentpickup;
 
+    void Start()
+    {
+        float randomrotation = Random.Range(0f, 360f);
+        print(randomrotation);
+    }
+
     public void instPickup(Vector3 pos)
     {
         if (currentpickup == null)
@@ -13,9 +19,9 @@ public class Pickup : MonoBehaviour
             currentpickup = Instantiate(pickupmodel);
         }
         currentpickup.transform.localPosition = new Vector3(pos.x, pickupmodel.transform.position.y, pos.z);
-        float randomrotation = Random.Range(0, 360);
+        float randomrotation = Random.Range(0f, 360f);
         currentpickup.transform.localRotation =  Quaternion.Euler(new Vector3(0, randomrotation, 0));
-        //Debug.Log(randomrotation);
+        Debug.Log(randomrotation);
     }
 
     public void destroyPickup()
