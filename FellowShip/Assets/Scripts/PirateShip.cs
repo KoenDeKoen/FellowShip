a4f4e3a7229b7e516000b4398d2ef1fdcd463cbd
martijnk1992 <Martijn.Koekkoek2@hva.nl>
2015-12-15 14:40:16 +0100

    pirateshipShit

--- FellowShip/Assets/Scripts/PirateShip.cs
+++ FellowShip/Assets/Scripts/PirateShip.cs
@@ -1,21 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class PirateShip : MonoBehaviour {
 	
 	public Transform enemyShip;
 	public Transform targetPlayer;
+
+	public PirateshipSpawn pss;
+
 	float distance;
-	float attackRange;
 	float moveSpeed;
 	float friction;
+	//float attackRange;
+	public static int state;
 
 	// Use this for initialization
 	void Start ()
 	{
-		attackRange = 200f;
+		//attackRange = 200f;
+		pss.Init();
 		moveSpeed = 20.0f;
 		friction = 5.0f;
+		state = 0;
 	}
 	
 	// Update is called once per frame
@@ -23,16 +31,32 @@ public class PirateShip : MonoBehaviour {
 	{
 		distance = Vector3.Distance(targetPlayer.position, enemyShip.position);
 
-		if(distance < attackRange)
+		switch(state)
 		{
-			LookAt();
+		case 1:
+			SpawnPirateShip();
+			state = 2;
+			break;
+			
+		case 2:
+			LookAt(targetPlayer);
 			Attack();
+			break;
+			
+		case 3:
+			LookAt(pss.returnSpawnPointPirate()[Random.Range(0, pss.returnSpawnPointPirate().Count)]);
+			Attack();
+			if(distance < 5.0F)
+			{
+				Destroy(enemyShip);
+			}
+			break;
 		}
 	}
 
-	public void LookAt()
+	public void LookAt(Transform target)
 	{
-		Quaternion rotation = Quaternion.LookRotation(targetPlayer.position - enemyShip.position);
+		Quaternion rotation = Quaternion.LookRotation(target.position - enemyShip.position);
 		enemyShip.rotation = Quaternion.Slerp(enemyShip.rotation, rotation, Time.deltaTime * friction);
 	}
 
@@ -40,4 +64,13 @@ public class PirateShip : MonoBehaviour {
 	{
 		enemyShip.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 	}
+
+	public void SpawnPirateShip()
+	{
+		int placePos = Random.Range (0, pss.returnSpawnPointPirate().Count);
+
+		Transform clone = Instantiate(enemyShip, pss.returnSpawnPointPirate()[placePos].transform.position, Quaternion.identity) as Transform;
+	}
+
+
 }
---
+++ FellowShip/Assets/Scripts/PirateshipSpawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PirateshipSpawn : MonoBehaviour {
+
+	private List<Transform> spawnPos;
+
+	public Transform pos1;
+	public Transform pos2;
+	public Transform pos3;
+	public Transform pos4;
+
+	public void Init()
+	{
+		spawnPos = new List<Transform> ();
+		spawnPos.Add (pos1);
+		spawnPos.Add (pos2);
+		spawnPos.Add (pos3);
+		spawnPos.Add (pos4);
+	}
+
+	public List<Transform> returnSpawnPointPirate()
+	{
+		return spawnPos;
+	}
+}
