67dff949eb3dbcfbcef3035f2a3ba19cb0aa5e7a
martijnk1992 <Martijn.Koekkoek2@hva.nl>
2016-01-15 09:58:37 +0100

    CompassIsDone

--- FellowShip/Assets/Scripts/CompassAim.cs
+++ FellowShip/Assets/Scripts/CompassAim.cs
@@ -3,55 +3,91 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class CompassAim : MonoBehaviour {
-	
+
 	public Transform compass;
+
+	public bool goalsAvailable;
 
 	GameObject currentBoat;
 	GameObject currentPickUp;
-	public bool goalsAvailable;
+	GameObject focusRotate;
 
 	public BoatUpgrade bu;
-	public PickUpSpawning pus;
 	public Pickup pu;
-
 
 	void Start()
 	{
+		focusRotate = new GameObject();
 		goalsAvailable = false;
 		currentBoat = bu.currentboat;
+		focusRotate.transform.position = currentBoat.transform.position;
 	}
 
-	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		if(goalsAvailable)
 		{
 			currentPickUp = pu.currentpickup;
-			//currentPickUp.transform.position = pus.currentPos;
 			currentBoat = bu.currentboat;
-			AimAtObjective();
+			focusRotate.transform.position = currentBoat.transform.position;
+			LookAt();
 		}
 	}
 
-//	public void AimAtObjective()
-//	{
-//		Quaternion rotation = Quaternion.LookRotation(currentBoat.transform.position - currentPickUp.transform.position);
-//		Debug.Log(rotation);
-//		compass.rotation = Quaternion.Slerp(compass.rotation, rotation, Time.deltaTime * 5.0f);
-//	}
-
-	public void AimAtObjective()
+	public void LookAt()
 	{
-		Vector3 target = currentPickUp.transform.position - currentBoat.transform.position;
-		Vector3 player = currentBoat.transform.right;
-		
-		float angle = Vector3.Angle(player, target);
-		Vector3 cross = Vector3.Cross(player, target);
-		
-		if(cross.y > 0f)
-		{
-			angle = -angle;
-		}
-		
+		Quaternion rot = Quaternion.LookRotation(currentPickUp.transform.position - focusRotate.transform.position);
+		focusRotate.transform.rotation = Quaternion.Slerp(focusRotate.transform.rotation, rot, Time.deltaTime * 10.0f);
+
+		float angle = focusRotate.transform.eulerAngles.y;
+		angle = -angle;
+
 		compass.rotation = Quaternion.Euler(0, 0, angle);
 	}
+//	public Transform compass;
+//
+//	Transform currentBoat;
+//	GameObject currentPickUp;
+//
+//	public bool goalsAvailable;
+//	public BoatUpgrade bu;
+//	public PickUpSpawning pus;
+//	public Pickup pu;
+//	
+//	void Start()
+//	{
+//		goalsAvailable = false;
+//		currentBoat = bu.currentboat;
+//	}
+//
+//	// Update is called once per frame
+//	void Update () {
+//		if(goalsAvailable)
+//		{
+//			currentPickUp = pu.currentpickup;
+//			currentBoat = bu.currentboat;
+//		
+//			AimAtObjective();
+//		}
+//	}
+//
+//	public void AimAtObjective()
+//	{
+//		Vector3 target = currentPickUp.transform.position;
+//		Vector3 player = currentBoat.transform.position;
+//		Debug.DrawLine(target, player);
+//
+//		Debug.Log("target" + target);
+//		Debug.Log("player" + player);
+//
+//		float angle = Vector3.Angle(player, target);
+//		Vector3 cross = Vector3.Cross(player, target);
+//
+//		if(cross.y > 0f)
+//		{
+//			angle = -angle;
+//		}
+//		
+//		compass.rotation = Quaternion.Euler(0, 0, angle);
+//	}
 }
