ce6d5e5bcbbc95feceb725f16d53a30a09f1bbc0
kleton5 <iankleton@live.nl>
2016-02-02 15:58:21 +0100

    Revert "Revert "Nieuw water, haai in de map en camera fix voor water""
    This reverts commit 05499af414acb7c890862b7f24bf3407afc6865e.

---
+++ FellowShip/Assets/Scripts/GolfScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GolfScript : MonoBehaviour {
+    public MeshFilter Meshfilter;
+    public float Smooth;
+    public float Density;
+	// Use this for initialization
+	void Start () {
+        Meshfilter = transform.GetComponent<MeshFilter>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Mesh M = Meshfilter.mesh;
+        Vector3[] V = M.vertices;
+        for (int i = 0; i < V.Length; i++)
+        {
+            V[i].y = Mathf.Sin(Time.time * Vector2.Distance(new Vector2(V[i].x, V[i].z), Vector2.zero) * Density) * Smooth;
+        }
+        M.vertices = V;
+        Meshfilter.mesh = M;
+	}
+}
