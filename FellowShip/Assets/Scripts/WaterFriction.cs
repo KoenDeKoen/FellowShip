59186ece810ba68b7920e7be929e0a6f69ca201d
kleton5 <iankleton@live.nl>
2016-02-02 15:38:39 +0100

    Nieuw water, haai in de map en camera fix voor water

---
+++ FellowShip/Assets/Scripts/WaterFriction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterFriction : MonoBehaviour {
+    public float Strength;
+    private bool swap = false;
+	void Update ()
+    {
+        if (swap == false)
+        {
+            transform.Rotate(0, 0, Strength);
+            if (transform.rotation.z > 195)
+            {
+                swap = true;
+            }
+        }
+
+        if (swap == true)
+        {
+            transform.Rotate(0, 0, -Strength);
+            if (transform.rotation.z < 165)
+            {
+                swap = false;
+            }
+        }
+    }
+}
